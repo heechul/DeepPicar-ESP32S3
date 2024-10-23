d02fd447af6414b60371bffb64521182df5670e5
Heechul Yun <heechul.yun@ku.edu>
2024-10-23 10:48:33 -0500

    limit the buffer size to 1 so that we always read the latest frame

--- camera-esp32web.py
+++ camera-esp32web.py
@@ -17,9 +17,14 @@ def init(res=(160, 120), fps=30, threading=True):
     global cap, use_thread, frame, cam_thr
     cam_res = 1
     cap = cv2.VideoCapture(URL + ":81/stream")
+
+    # Set buffer size to 1
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
     if not cap.isOpened():
         print ("Cannot open camera.")
         return    
+
     # 0 - 96x96, 1 - 160x120, 2 - 176x144, 3 - 320x240, 4 - 352x288, 
     # 5 - 640x480, 6 - 800x600, 7 - 1024x768, 8 - 1280x1024, 9 - 1600x1200
     if res == (96, 96): cam_res = 0
@@ -29,6 +34,7 @@ def init(res=(160, 120), fps=30, threading=True):
     else:
         print ("Camera resolution not supported.")
         return
+
     requests.get(URL + "/control?var=framesize&val={}".format(cam_res)) 
     
     # start the camera thread
